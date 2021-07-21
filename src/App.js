97f0fd207586ef80a1c364ae8e914c7cfa82a773
AbdelrhmanAmin <maxhd213@gmail.com>
2021-07-21 06:15:45 +0200

    add api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  componentDidMount = () => {
+    fetch(`${process.env.REACT_APP_API}/users/2`)
+      .then(res => res.json())
+      .then(user => document.getElementById('name').innerHTML = user.name)
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div id='name'></div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
