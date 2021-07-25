41a8fa07f99eef1fbcda0caaf7dddf2a56584d79
AbdelrhmanAmin <maxhd213@gmail.com>
2021-07-25 03:02:02 +0200

    floating animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,21 +9,39 @@ import {
   Link, useHistory
 } from 'react-router-dom';
 import logo from '../assets/logo.png'
+import apple from '../assets/logo-apple.png'
+import banana from '../assets/logo-banana.png'
+import brocoli from '../assets/logo-brocoli.png'
+import grape from '../assets/logo-grape.png'
+import tomato from '../assets/logo-tomato.png'
 
 const Home = ({ currentUser, cart, products }) => {
   const dispatch = useDispatch()
   const history = useHistory()
   const [visible, setVisible] = useState(false)
   const [productX, setProduct] = useState(0)
+  let logos = [logo, banana, apple, grape, brocoli, tomato]
+  let i = 0
   useEffect(() => {
     dispatch(fetchOrders(currentUser.id))
     dispatch(fetchProducts())
+    setInterval(() => {
+      document.getElementById('logo').src = logos[i]
+      i += 1
+      if (i === logos.length) {
+        i = 0
+      }
+    }, 3000)
   }, [currentUser])
   return (
     <div>
       <div className='nav-flex'>
         <div>
-          <Link to='/'><img src={logo} width='75' height='75' alt='logo' /></Link>
+          <Link to='/' className='logo-container'>
+            <img src={logo} width='75' height='75' alt='logo' className='logo l-1' id='logo' />
+            <h3>Vegan</h3>
+            <h2>Market</h2>
+          </Link>
         </div>
         <div className='nav-flex-right'>
           <Link to='/cart'><FaCartArrowDown color='white' fontSize='2.5rem' /></Link>
