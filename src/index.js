06ff47d8c327a87c480d02dfc47a0ca923a00083
AbdelrhmanAmin <maxhd213@gmail.com>
2021-08-25 02:50:14 +0200

    deploying..

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './Reducers/index';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
